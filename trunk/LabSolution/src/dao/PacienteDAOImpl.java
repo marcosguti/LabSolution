295ebabe8ff248f533ba333e24df6b6f570e8853
marcosguti2000 <marcosguti2000@d16d29b2-7557-44a6-8a3d-6a64d6105533>
2016-02-22 03:24:53 +0000

    consulta de pacients por nombre y cedula, interfaz de resultado, eviar datos de la tabla pacieentes al jframe resultado

--- trunk/LabSolution/src/dao/PacienteDAOImpl.java
+++ trunk/LabSolution/src/dao/PacienteDAOImpl.java
@@ -10,62 +10,66 @@ import clases.Paciente;
 
 import hibernateUtil.BussinessException;
 
-
 public class PacienteDAOImpl extends GenericDAOImplHibernate<Paciente, Integer> implements PacienteDAO {
 
-	@Override
-	public List< Paciente> getByCed(String cedula) throws BussinessException {
-		
-		Session session = sessionFactory.getCurrentSession();
-                        session.beginTransaction();
-		try {
-			Query query = 
-					session.createQuery(
-							"SELECT e FROM Paciente e WHERE e.cedula = ?");
-			query.setParameter(0, cedula);
-			List<Paciente> paciente = (List<Paciente>) query.list();
-			return paciente;
-			
-			
-			
-//			session.beginTransaction();
-//			Paciente entity = (Paciente) session.get(, cedula);
-//			session.getTransaction().commit();
-//
-//			return entity;
-		} catch (javax.validation.ConstraintViolationException cve) {
-			try {
-				if (session.getTransaction().isActive()) {
-					session.getTransaction().rollback();
-				}
-			} catch (Exception exc) {
-exc.printStackTrace()	;		}
-			throw new BussinessException(cve);
-		} catch (org.hibernate.exception.ConstraintViolationException cve) {
-			try {
-				if (session.getTransaction().isActive()) {
-					session.getTransaction().rollback();
-				}
-			} catch (Exception exc) {
-				exc.printStackTrace();			}
-			throw new BussinessException(cve);
-		} catch (RuntimeException ex) {
-			try {
-				if (session.getTransaction().isActive()) {
-					session.getTransaction().rollback();
-				}
-			} catch (Exception exc) {
-				exc.printStackTrace();					}
-			throw ex;
-		} catch (Exception ex) {
-			try {
-				if (session.getTransaction().isActive()) {
-					session.getTransaction().rollback();
-				}
-			} catch (Exception exc) {
-				exc.printStackTrace();					}
-			throw new RuntimeException(ex);
-		}
-	}
+    @Override
+    public Paciente getByNombreCedula(String nombre, String cedula) throws BussinessException {
+
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        try {
+            Query query
+                    = session.createQuery(
+                            "SELECT e FROM Paciente e WHERE e.cedula = ?");
+//            query.setParameter(0, nombre);
+            query.setParameter(0, cedula);
+            Paciente paciente = (Paciente) query.uniqueResult();
+            session.getTransaction().commit();
+            if (paciente != null) {
+                return paciente;
+            }
+            return null;//			session.beginTransaction();
+            //			Paciente entity = (Paciente) session.get(, cedula);
+            //			session.getTransaction().commit();
+            //
+            //			return entity;
+        } catch (javax.validation.ConstraintViolationException cve) {
+            try {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
+            throw new BussinessException(cve);
+        } catch (org.hibernate.exception.ConstraintViolationException cve) {
+            try {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
+            throw new BussinessException(cve);
+        } catch (RuntimeException ex) {
+            try {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
+            throw ex;
+        } catch (Exception ex) {
+            try {
+                if (session.getTransaction().isActive()) {
+                    session.getTransaction().rollback();
+                }
+            } catch (Exception exc) {
+                exc.printStackTrace();
+            }
+            throw new RuntimeException(ex);
+        }
+    }
 
 }
